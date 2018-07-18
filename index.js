664d065562826b483396d92d78d2f2be2c4d750d
Lise Ferguson <lise.ferguson503@gmail.com>
2018-07-18 15:35:23 -0700

    updated to show correct error message

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 /* This app doesn't follow a11y best practices, and the JS file is incomplete. Complete the getDataFromApi and displaySearchData functions. When you're done, this app should allow a user to search for a specific dog breed, and display a random image of that dog breed. You should make requests to this API: https://dog.ceo/dog-api/ . Also make any necessary adjustments to make this app accessible. */
 
-//how to connect searchTerm to user input?
-//what is the callback in this code and what is being called back?
+//this function sends a data request to the API. The const "settings" is an object with params defined in the documentation.
+//Q: how to search breed and sub-breed
+//how to return multiple images, limit how many
+//how to have an error that says when there are no results
 function getDataFromApi(searchTerm, callback) {
   console.log(searchTerm);
   const settings = {
@@ -10,45 +12,32 @@ function getDataFromApi(searchTerm, callback) {
     type: 'GET',
     success: callback,
     error: function(error){
-      console.log(error);
+      alert("please enter a valid breed");
     }
-  }
+    }
 $.ajax(settings);
 }
 
-//how to select the image from the data
-//data is not defined, how does code know what it is
+//??? is this the callback function because it is passed into the bottom of the watchSubmit function as an argument?
+//data param refrences searchTerm from above function
 function displaySearchData(data) {
   console.log(data);
-  let htmlToDisplay = renderResults(data);
-  $('.js-search-results').html(htmlToDisplay);
-}
-
-function renderResults(dataPassedToThisFunction){
-  return `
+   let results = `
   <div class ="image-container">
-    <href="https://dog.ceo/api/breed/${dataPassedToThisFunction.message}/image/random" alt="photo of chosen dog breed" target="_blank">
+    <img src="${data.message}" alt='photo of your dog' target="_blank">
   </div>
-  `;
+  `
+  $('.js-search-results').html(results);
 }
-/* <div>
-      <h2>${$('.js-query-title').val()}</h2>
-      by
-      <h2>${$('.js-query-artist').val()}</h2>
-      <p class="result-lyrics" href="https://api.lyrics.ovh/v1/${'.js-query-artist'}/${'.js-query-title'} target="_blank">${dataPassedToThisFunction.lyrics}</p>
-    </div>
-*/
 
+//this function contains the event listener connected to the search button. When the user hits search, preventDefault prevents redirection to another page, and then the getDataFromApi function is called.
 function watchSubmit() {
   $('.js-search-form').submit(event => {
     event.preventDefault();
-    //currentTarget
-    let queryTarget = $(event.currentTarget).find('.js-query');
-    let query = queryTarget.val();
-    // clear out the input
-    queryTarget.val("");
-    getDataFromApi(query, displaySearchData);
+    let searchTerm = $('.js-query').val();
+    getDataFromApi(searchTerm, displaySearchData);
   });
 }
 
+//this fucntion waits for the page to load, then calls all the functions inside it above
 $(watchSubmit);
